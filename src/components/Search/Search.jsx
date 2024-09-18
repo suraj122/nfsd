60d352c86fa5347b349b153f15ccc6188eb7823f
Mukesh kumar <122suraj0@gmail.com>
2024-09-18 12:23:34 +0530

    implement caching and debouncing in search app

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,18 +3,30 @@ import { useEffect, useState } from "react";
 const Search = () => {
   const [searchText, setSearchText] = useState("");
   const [searchData, setSearchData] = useState([]);
+  const [isSearchVisible, setIsSearchListVisible] = useState(false);
+  const [cacheResult, setCacheResult] = useState({});
 
   useEffect(() => {
-    fetchData();
+    const i = setTimeout(() => {
+      fetchData();
+    }, 300);
+    return () => clearTimeout(i);
   }, [searchText]);
 
   const fetchData = async () => {
-    const data = await fetch(
-      `https://www.google.com/complete/search?client=firefox&q=${searchText}`
-    );
-    const json = await data.json();
-    setSearchData(json[1]);
+    if (cacheResult[searchText]) {
+      setSearchData(cacheResult[searchText]);
+    } else {
+      const data = await fetch(
+        `https://www.google.com/complete/search?client=firefox&q=${searchText}`
+      );
+      const json = await data.json();
+      setCacheResult((prev) => ({ ...prev, [searchText]: json[1] }));
+      setSearchData(json[1]);
+    }
   };
+
+  console.log(cacheResult);
 
   return (
     <div className="max-w-xl  mx-auto px-16 py-14">
@@ -24,9 +36,11 @@ const Search = () => {
           type="text"
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
+          onFocus={() => setIsSearchListVisible(true)}
+          onBlur={() => setIsSearchListVisible(false)}
         />
       </form>
-      {searchData.length > 0 && (
+      {searchData.length > 1 && isSearchVisible && (
         <ul className="border rounded-md border-black p-2">
           {searchData.map((s) => (
             <li key={s}>{s}</li>
